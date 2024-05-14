c5019ff7f6f7f5faa8432d8113523e5bd3030f23
Tahira-Fatimah <tahirafatimah27@gmail.com>
2024-05-15 03:17:35 +0500

    modified exploreController

--- backend/controllers/exploreController.js
+++ backend/controllers/exploreController.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const getLatestStories = async (req, res) => {
     try {
-        const latestStories = await Story.find().sort({ createdAt: -1 }).limit(10); 
+        const latestStories = await Story.find().sort({ createdAt: -1 }).limit(10).populate('initiator');
         res.status(200).json({ latestStories });
     } catch (err) {
         res.status(400).json({ error: err.message });
@@ -15,10 +15,10 @@ const getLatestStories = async (req, res) => {
 
 const getLikesForStory = async (storyId) => {
     try {
-        const story = await Story.findById( storyId )
-        const chapters = story.chapters; 
-       
-        const likes = await Liked.countDocuments({ likedChapter: {$in : chapters}, type:"like" });
+        const story = await Story.findById(storyId)
+        const chapters = story.chapters;
+
+        const likes = await Liked.countDocuments({ likedChapter: { $in: chapters }, type: "like" });
 
         return likes;
     } catch (err) {
@@ -58,32 +58,31 @@ const DEFAULT_COVER_PATH = path.join(__dirname, '..', 'images', 'genreCover', 'd
 const sendDefaultCover = (res) => {
     const imageStream = fs.createReadStream(DEFAULT_COVER_PATH);
     imageStream.on('open', () => {
-        res.set('Content-Type', 'image/jpeg'); 
-        imageStream.pipe(res); 
+        res.set('Content-Type', 'image/jpeg');
+        imageStream.pipe(res);
     });
     imageStream.on('error', (err) => {
         res.status(500).json({ error: err });
     });
 };
 
+
 const getGenreCover = async (req, res) => {
     try {
-        const { genre } = req.query; 
+        const { genre } = req.query;
 
         const imagePath = path.join(__dirname, '..', 'images', 'genreCover', `${genre}.jpg`);
 
         fs.access(imagePath, fs.constants.F_OK, (err) => {
-            if (err) 
-            {
+            if (err) {
                 console.log(`Genre cover for ${genre} not found, returning default cover.`);
                 sendDefaultCover(res);
-            } 
-            else 
-            {
+            }
+            else {
                 const imageStream = fs.createReadStream(imagePath);
                 imageStream.on('open', () => {
-                    res.set('Content-Type', 'image/jpeg'); 
-                    imageStream.pipe(res); 
+                    res.set('Content-Type', 'image/jpeg');
+                    imageStream.pipe(res);
                 });
                 imageStream.on('error', (err) => {
                     res.status(500).json({ error: err });
@@ -96,16 +95,32 @@ const getGenreCover = async (req, res) => {
 };
 
 const getGenres = async (req, res) => {
-    try 
-    {
-        const genres = await Story.distinct('genre');
+    try {
+        const distinctGenres = await Story.distinct('genre');
+        let genres = [];
+
+
+        for (const genre of distinctGenres) {
+
+            const stories = await Story.find({ genre: genre }).populate('chapters');
+
+            let reads = 0
+            for (const story of stories) {
+                for (const chapter of story.chapters) {
+                    console.log(chapter)
+                    reads += chapter.readBy.length
+                }
+            }
+            genres.push({ genre: genre, count: stories.length, reads: reads });
+
+        }
+
         res.status(200).json({ genres });
-    } 
-    catch (err) 
-    {
-        res.status(400).json({ error: err.message });
+    } catch (err) {
+        res.status(500).json({ error: err });
     }
 };
+
 
 
 const getUserLikedAndReadStoryGenres = async (userId) => {
@@ -125,23 +140,23 @@ const getUserLikedAndReadStoryGenres = async (userId) => {
         return genres;
     } catch (err) {
         console.error(err);
-        return new Set(); 
+        return new Set();
     }
 };
 
-const getTopPicks  = async(req, res)=>{
-    try{
+const getTopPicks = async (req, res) => {
+    try {
 
         const userGenres = await getUserLikedAndReadStoryGenres(req.user.id)
         const user = await User.findById(req.user.id)
- 
 
-        const topPicks = await Story.find({genre: {$in:userGenres}, _id:{ $nin: user.initiatedStories}}).sort({ createdAt: -1 }).limit(10); 
 
-        res.status(200).json({topPicks:topPicks })
+        const topPicks = await Story.find({ genre: { $in: userGenres }, _id: { $nin: user.initiatedStories } }).sort({ createdAt: -1 }).limit(10);
+
+        res.status(200).json({ topPicks: topPicks })
     }
-    catch(err){
-        res.status(400).json({error: err})
+    catch (err) {
+        res.status(400).json({ error: err })
     }
 }
 
@@ -154,11 +169,11 @@ const calculateLikesForUser = async (user) => {
 
     const chapters = await Chapter.find({
         author: user._id,
-        createdAt: { $gte: pastMonth}
+        createdAt: { $gte: pastMonth }
     });
     const chapterIds = chapters.map(chapter => chapter._id);
 
-    const totalLikes = await Liked.countDocuments({likedChapter: { $in: chapterIds }, type:"like"})
+    const totalLikes = await Liked.countDocuments({ likedChapter: { $in: chapterIds }, type: "like" })
 
     return totalLikes > 0 ? totalLikes : 0;
 };
@@ -177,9 +192,8 @@ const getWriterOfTheMonth = async (req, res) => {
                 writerOfTheMonth = user;
             }
         }
-
         if (writerOfTheMonth) {
-            res.status(200).json({ writerOfTheMonth });
+            res.status(200).json({ writerOfTheMonth: writerOfTheMonth, writtenChapters: writerOfTheMonth.writtenChapters.length });
         } else {
             res.status(404).json({ message: "No writers found" });
         }
@@ -188,7 +202,37 @@ const getWriterOfTheMonth = async (req, res) => {
     }
 };
 
+const getBestSeller = async (req, res) => {
+    try {
+
+        const bestSellers = []
+        const mostBoughtStories = await Story.aggregate([
+            {
+                $addFields: {
+                    boughtByCount: { $size: "$boughtBy" }
+                }
+            },
+            {
+                $sort: { "boughtByCount": -1 }
+            }
+        ]).populate('chapters');
+
+        for (const story of mostBoughtStories) {
+            let reads = 0
+            for (const chapter of story.chapters) {
+                console.log(chapter)
+                reads += chapter.readBy.length
+            }
+            bestSellers.push({ story: story, reads: reads })
+        }
+        return res.status(200).json({ bestSellers: bestSellers })
+    }
+    catch (error) {
+        return res.status(500).json({ error: error })
+    }
+}
 
 
 
-module.exports = {getLatestStories, getTrendingStories, getGenres, getTopPicks, getWriterOfTheMonth, getGenreCover};
+
+module.exports = { getLatestStories, getTrendingStories, getGenres, getTopPicks, getWriterOfTheMonth, getGenreCover,getBestSeller };
