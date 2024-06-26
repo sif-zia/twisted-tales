7f4697a5962cdeccb67e5cb01d71efbd2dec4d84
Sif Zia <56488566+sif-zia@users.noreply.github.com>
2024-06-26 04:13:00 -0700

    Dockerized both backend and front end

--- backend/utilities/getImage.js
+++ backend/utilities/getImage.js
@@ -4,17 +4,23 @@ const path = require('path');
 const getImage = async (req, res) => {
     try {
         const { imagePath } = req.query; // Use req.query for GET requests
+        
+        // Normalize the path to handle cross-platform differences
+        const normalizedPath = path.normalize(imagePath).split("\\").join('/');
+        const resolvedImagePath = path.join(__dirname, '..', normalizedPath);
 
-        fs.access(imagePath, fs.constants.F_OK, (err) => {
+        console.log('Path Requested', resolvedImagePath);
+
+        fs.access(resolvedImagePath, fs.constants.F_OK, (err) => {
             if (err) {
                 return res.status(404).json({ error: 'Image not found' });
             }
 
-            const ext = path.extname(imagePath).toLowerCase();
+            const ext = path.extname(resolvedImagePath).toLowerCase();
             const contentType = ext === '.jpg' || ext === '.jpeg' ? 'image/jpeg' : 'image/png';
 
             res.setHeader('Content-Type', contentType);
-            const imageStream = fs.createReadStream(imagePath);
+            const imageStream = fs.createReadStream(resolvedImagePath);
             imageStream.pipe(res);
             imageStream.on('error', (err) => {
                 res.status(500).json({ error: err });
